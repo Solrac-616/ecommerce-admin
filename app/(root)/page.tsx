2b05342524cd42e815edffb0794274098862f453
Carlos Brito <carlos.e1998g@gmail.com>
2023-07-05 20:51:43 -0400

    prueba boton

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="p-4">
         <h2>ADMIN DASHBOARD</h2>
+        <Button>Click me</Button>
       </div>
     </>
   )
